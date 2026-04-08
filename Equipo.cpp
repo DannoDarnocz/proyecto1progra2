8d45a7530d1ca5502414b767f7d4cc8d439cfe25
Darnocz <46076620+DannoDarnocz@users.noreply.github.com>
2026-04-08 16:50:57 -0600

    Inicio del proeycto

---
+++ Equipo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel on 08/04/2026.
+//
+
+#include "Equipo.h"
---
+++ Equipo.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel on 08/04/2026.
+//
+
+#ifndef PROYECTO_1_EQUIPO_H
+#define PROYECTO_1_EQUIPO_H
+
+
+class Equipo
+{
+};
+
+
+#endif //PROYECTO_1_EQUIPO_H
